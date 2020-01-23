d2fabd8988d2ba78ff43095e5e1e6b9c34d3237d
samyemadbbn <samy.emad@businessboomers.net>
2020-01-23 14:46:04 +0200

    edit connection

--- connection/config.php
+++ connection/config.php
@@ -3,7 +3,7 @@ define("DB_SERVER",'localhost');
 define("DB_NAME",'test_task');
 define("DB_USER",'root');
 define("DB_PASS",'root');
-define('API_KEY','test');
+define('API_KEY','AIzaSyDJ_Eqp9guyY00NnGDcHLf6bIGaBrl8_O8');
 $root = realpath($_SERVER["DOCUMENT_ROOT"]) . "/test";
 define("real_root",$root);    //
 
